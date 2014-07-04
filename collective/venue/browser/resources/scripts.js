9aa14ebb6a3df2d0f7dbd2e3de735a0b89639cf3
Johannes Raggam <raggam-nl@adm.at>
2014-07-04 10:08:29 +0200

    autoformat

--- collective/venue/browser/resources/scripts.js
+++ collective/venue/browser/resources/scripts.js
@@ -1,73 +1,78 @@
-    function initialize_map() {
-        // Initialize the map
+function initialize_map() {
+    // Initialize the map
 
-        if ($('#map').length === 0) { return; }
-
-        var editable = $('div.geolocation_wrapper.edit').length && true || false;
-
-        var map = new L.Map("map", {});
-
-        L.tileLayer.provider('OpenStreetMap.DE').addTo(map);
-        var baseLayers = ['OpenStreetMap.DE', 'Esri.WorldImagery', 'Esri.WorldStreetMap', 'OpenCycleMap'];
-        var layerControl = L.control.layers.provided(baseLayers).addTo(map);
-
-        var fullScreen = new L.Control.FullScreen();
-        map.addControl(fullScreen);
-
-        // ADD MARKERS
-        var markers = new L.MarkerClusterGroup();
-        $('div.geolocation').each(function() {
-            var geo = $(this).data();
-            var marker = new L.Marker([geo.latitude, geo.longitude],
-                                  {draggable: editable});
-            marker.bindPopup(geo.description);
-            if (editable) {
-                marker.on('dragend', function (e) {
-                    var coords = e.target.getLatLng();
-                    update_inputs(coords.lat, coords.lng);
-                });
-            }
-            markers.addLayer(marker);
-        });
-        map.addLayer(markers);
-
-        // autozoom
-        var bounds = markers.getBounds();
-        map.fitBounds(bounds);
-
-        if (editable) {
-            var update_inputs = function(lat, lng) {
-                var map_wrap = $('#map').closest('div.geolocation_wrapper.edit');
-                map_wrap.find('input.latitude').attr('value', lat);
-                map_wrap.find('input.longitude').attr('value', lng);
-            };
-            map.on('geosearch_showlocation', function (e) {
-                var coords = e.Location;
-                update_inputs(coords.Y, coords.X);
-            });
-
-            // GEOSEARCH
-            var geosearch = new L.Control.GeoSearch({
-                draggable: editable,
-                provider: new L.GeoSearch.Provider.Google()
-                //provider: new L.GeoSearch.Provider.OpenStreetMap()
-            }).addTo(map);
-        }
-
+    if ($('#map').length === 0) {
+        return;
     }
+
+    var editable = $('div.geolocation_wrapper.edit').length && true || false;
+
+    var map = new L.Map("map", {});
+
+    L.tileLayer.provider('OpenStreetMap.DE').addTo(map);
+    var baseLayers = ['OpenStreetMap.DE', 'Esri.WorldImagery', 'Esri.WorldStreetMap', 'OpenCycleMap'];
+    var layerControl = L.control.layers.provided(baseLayers).addTo(map);
+
+    var fullScreen = new L.Control.FullScreen();
+    map.addControl(fullScreen);
+
+    // ADD MARKERS
+    var markers = new L.MarkerClusterGroup();
+    $('div.geolocation').each(function() {
+        var geo = $(this).data();
+        var marker = new L.Marker([geo.latitude, geo.longitude], {
+            draggable: editable
+        });
+        marker.bindPopup(geo.description);
+        if (editable) {
+            marker.on('dragend', function(e) {
+                var coords = e.target.getLatLng();
+                update_inputs(coords.lat, coords.lng);
+            });
+        }
+        markers.addLayer(marker);
+    });
+    map.addLayer(markers);
+
+    // autozoom
+    var bounds = markers.getBounds();
+    map.fitBounds(bounds);
+
+    if (editable) {
+        var update_inputs = function(lat, lng) {
+            var map_wrap = $('#map').closest('div.geolocation_wrapper.edit');
+            map_wrap.find('input.latitude').attr('value', lat);
+            map_wrap.find('input.longitude').attr('value', lng);
+        };
+        map.on('geosearch_showlocation', function(e) {
+            var coords = e.Location;
+            update_inputs(coords.Y, coords.X);
+        });
+
+        // GEOSEARCH
+        var geosearch = new L.Control.GeoSearch({
+            draggable: editable,
+            provider: new L.GeoSearch.Provider.Google()
+            //provider: new L.GeoSearch.Provider.OpenStreetMap()
+        }).addTo(map);
+    }
+
+}
 
 
 (function($) {
 
-    $(document).ready(function () {
+    $(document).ready(function() {
         initialize_map();
         // Open location view in popup.
-        if(typeof common_content_filter!='undefined' && 'prepOverlay' in $) {
+        if (typeof common_content_filter != 'undefined' && 'prepOverlay' in $) {
             var overlay_opts = {
                 subtype: 'ajax',
                 filter: common_content_filter,
                 cssclass: 'overlay-venue',
-                config: { onLoad: initialize_map }
+                config: {
+                    onLoad: initialize_map
+                }
             }
             $('a.venue_ref_popup').prepOverlay(overlay_opts);
         }
