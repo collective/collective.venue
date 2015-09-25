b7784742098ccc9527e9c57de941e0788ffbc797
Johannes Raggam <raggam-nl@adm.at>
2015-09-25 11:05:46 +0200

    update translations

--- build-i18n.sh
+++ build-i18n.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
-I18NDUDE=../../bin/i18ndude
+I18NDUDE=i18ndude
 I18NPATH=collective/venue
 DOMAIN=collective.venue
 $I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --create $DOMAIN $I18NPATH
 $I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po
-
-## Don't automatically run the rebuild for the "plone" domain, as it puts too
-## much message strings into it.
-#DOMAIN=plone
-#$I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --merge $I18NPATH/locales/merge-plone.pot --create $DOMAIN $I18NPATH
-#$I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po
-
