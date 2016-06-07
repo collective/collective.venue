40410ac5fe1973a3ad2d776cba8de869f8bb3e9a
Johannes Raggam <thetetet@gmail.com>
2016-06-08 01:28:33 +0200

    refactor plone 5 upgrade step

--- collective/venue/upgrades.py
+++ collective/venue/upgrades.py
@@ -1,26 +1,34 @@
-from Products.CMFPlone.utils import getToolByName
-from Products.CMFPlone.utils import getFSVersionTuple
+# -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+import logging
 
 
+logger = logging.getLogger('collective.venue upgrade')
 PROFILE_ID = 'profile-collective.venue:default'
-PROFILE_ID_PLONE4 = 'profile-collective.venue:plone4'
-PROFILE_ID_PLONE5 = 'profile-collective.venue:plone5'
 
 
-def upgrade_resources(context, logger=None):
-    setup = getToolByName(context, 'portal_setup')
-    if getFSVersionTuple()[0] == 4:
-        setup.runImportStepFromProfile(PROFILE_ID, 'cssregistry')
-        setup.runImportStepFromProfile(PROFILE_ID, 'jsregistry')
-    else:
-        setup.runImportStepFromProfile(PROFILE_ID, 'plone.app.registry')
+def unregister_resource(registry, resource):
+    if registry and registry.getResource(resource):
+        registry.unregisterResource(resource)
+        logger.info("Removed {0} from {1}".format(resource, registry.id))
 
 
-def upgrade_registry(context, logger=None):
+def upgrade_3_to_4(context):
+    """Remove JS and CSS resources from portal_css and portal_js registry.
+    Import resource registry configuration.
+    """
+
+    # Unregister JavaScript
+    unregister_resource(
+        getToolByName(context, 'portal_javascripts'),
+        '++resource++collective.venue/scripts.js'
+    )
+
+    # Unregister CSS
+    unregister_resource(
+        getToolByName(context, 'portal_css'),
+        '++resource++collective.venue/styles.css'
+    )
+
     setup = getToolByName(context, 'portal_setup')
     setup.runImportStepFromProfile(PROFILE_ID, 'plone.app.registry')
-
-
-def upgrade_to_plone_5(context):
-    upgrade_registry(context)
-    upgrade_resources(context)
