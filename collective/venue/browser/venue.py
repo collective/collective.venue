b476e41d455569d00546c630dc6fbf7953931dd5
Johannes Raggam <thetetet@gmail.com>
2016-07-08 10:28:00 +0200

    use pat-leaflet including geojson data structure

--- collective/venue/browser/venue.py
+++ collective/venue/browser/venue.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-from Products.CMFPlone.utils import safe_unicode
-from Products.Five.browser import BrowserView
 from collective.address.behaviors import IAddress
 from collective.address.behaviors import ISocial
 from collective.address.vocabulary import get_pycountry_name
 from plone.app.uuid.utils import uuidToObject
-import pkg_resources
+from plone.uuid.interfaces import IUUID
+from Products.CMFPlone.utils import safe_unicode
+from Products.Five.browser import BrowserView
+
 import json
+import pkg_resources
+
 
 try:
     pkg_resources.get_distribution('collective.geolocationbehavior')
@@ -34,15 +37,36 @@ class VenueView(BrowserView):
         self.request = request
 
     @property
+    def title(self):
+        title = safe_unicode(getattr(self.context, 'title', u''))
+        return title
+
+    @property
+    def description(self):
+        description = safe_unicode(getattr(self.context, 'description', u''))
+        return description
+
+    @property
+    def data_coordinates(self):
+        if not HAS_GEOLOCATION:
+            return
+        geo = IGeolocatable(self.context, None)
+        if not geo:
+            return
+
+        return (
+            geo.geolocation.latitude,
+            geo.geolocation.longitude
+        )
+
+    @property
     def data(self):
         context = self.context
-        title = safe_unicode(getattr(self.context, 'title', u''))
-        description = safe_unicode(getattr(self.context, 'description', u''))
 
         address_data = {}
 
-        address_data['title'] = title
-        address_data['description'] = description
+        address_data['title'] = self.title
+        address_data['description'] = self.description
 
         add = IAddress(context, None)
         if add:
@@ -60,19 +84,41 @@ class VenueView(BrowserView):
             address_data['mobile'] = contact.mobile
             address_data['fax'] = contact.fax
 
-        geo = None
-        if HAS_GEOLOCATION:
-            geo = IGeolocatable(context, None)
-        if geo:
-            latitude = geo.geolocation.latitude
-            longitude = geo.geolocation.longitude
-            geo_json = json.dumps([{
-                'lat': latitude,
-                'lng': longitude,
-                'popup': u'<h3>{0}</h3><p>{1}</p>'.format(title, description)
-            }])
-            address_data['geopoints'] = geo_json
-            address_data['latitude'] = latitude
-            address_data['longitude'] = longitude
+        coordinates = self.data_coordinates
+        if coordinates:
+            address_data['latitude'] = coordinates[0]
+            address_data['longitude'] = coordinates[1]
 
         return address_data
+
+    @property
+    def data_geojson(self):
+        """Return the geo location as GeoJSON string.
+        """
+        coordinates = self.data_coordinates
+        if not coordinates:
+            return
+
+        geo_json = json.dumps({
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'id': IUUID(self.context),
+                    'properties': {
+                        'popup': u'<h3>{0}</h3><p>{1}</p>'.format(
+                            self.title,
+                            self.description
+                        )
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [
+                            coordinates[0],
+                            coordinates[1]
+                        ]
+                    }
+                },
+            ]
+        })
+        return geo_json
