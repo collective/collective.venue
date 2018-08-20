04e925ba22c3b89ab8f5c6571648624a708babb8
nzambello <nicola.zambello@redturtle.it>
2018-08-20 17:10:10 +0200

    completed collection tile view

--- collective/venue/resources/venue-collection.js
+++ collective/venue/resources/venue-collection.js
@@ -1,53 +1,55 @@
 $(function() {
-  var geoJson = JSON.parse(document.querySelector('.collection-leaflet-map').getAttribute('data-geojson'));
-  var categoriesLayers = {};
+  setTimeout(function() {
+    var geoJson = JSON.parse(document.querySelector('.collection-leaflet-map').getAttribute('data-geojson'));
+    var categoriesLayers = {};
 
-  var map = L.map(document.querySelector('.collection-leaflet-map'), {
-    fullscreenControl: true,
-    zoomControl: true,
-    sleep: false,
-    sleepNote: false,
-    hoverToWake: false,
-    sleepOpacity: 1
-  });
+    var map = L.map(document.querySelector('.collection-leaflet-map'), {
+      fullscreenControl: true,
+      zoomControl: true,
+      sleep: false,
+      sleepNote: false,
+      hoverToWake: false,
+      sleepOpacity: 1
+    });
 
-  var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
+    var tileLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-  var markerCluster = new L.MarkerClusterGroup();
-  var markerLayer = L.geoJson(geoJson, {
-    pointToLayer: function(feature, latlng) {
-      var marker = L.marker(latlng, {
-        icon: L.AwesomeMarkers.icon({
-          markerColor: 'red',
-          prefix: 'fa',
-          icon: 'circle',
-        }),
-        draggable: false,
-      }).bindPopup(feature.properties.popup).addTo(map);
+    var markerCluster = new L.MarkerClusterGroup();
+    var markerLayer = L.geoJson(geoJson, {
+      pointToLayer: function(feature, latlng) {
+        var marker = L.marker(latlng, {
+          icon: L.AwesomeMarkers.icon({
+            markerColor: 'red',
+            prefix: 'fa',
+            icon: 'circle',
+          }),
+          draggable: false,
+        }).bindPopup(feature.properties.popup).addTo(map);
 
-      feature.properties.categories.forEach(function(cat) {
-        if (!categoriesLayers[cat]) {
-          categoriesLayers[cat] = L.layerGroup();
-        }
+        feature.properties.categories.forEach(function(cat) {
+          if (!categoriesLayers[cat]) {
+            categoriesLayers[cat] = L.layerGroup();
+          }
 
-        marker.addTo(categoriesLayers[cat]);
-      });
+          marker.addTo(categoriesLayers[cat]);
+        });
 
-      return marker;
-    },
-  });
+        return marker;
+      },
+    });
 
-  for (var cat in categoriesLayers) {
-    map.addLayer(categoriesLayers[cat]);
-  }
+    for (var cat in categoriesLayers) {
+      map.addLayer(categoriesLayers[cat]);
+    }
 
-  markerCluster.addLayer(markerLayer);
-  map.fitBounds(markerCluster.getBounds());
+    markerCluster.addLayer(markerLayer);
+    map.fitBounds(markerCluster.getBounds());
 
-  if (Object.values(categoriesLayers).length > 0) {
-    L.control.layers({}, categoriesLayers, { collapsed: false }).addTo(map);
-  }
+    if (Object.values(categoriesLayers).length > 0) {
+      L.control.layers({}, categoriesLayers, { collapsed: false }).addTo(map);
+    }
+  }, 0);
 });
