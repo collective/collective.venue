a0cf7ab512ab6567e2939cdb4732814f7cefc8cf
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-01-21 15:02:32 +0100

    Remove registry initialization in bundle.

--- collective/venue/resources/collective-venue-bundle.js
+++ collective/venue/resources/collective-venue-bundle.js
@@ -1,16 +1,4 @@
-require([
-  'pat-registry',
-  'collective-venue'
-], function(registry) {
-  'use strict';
-
-  // initialize only if we are in top frame
-  if (window.parent === window) {
-    $(document).ready(function() {
-      if (!registry.initialized) {
-        registry.init();
-      }
-    });
-  }
+require(["pat-registry", "collective-venue"], function(registry) {
+  "use strict";
 
 });
