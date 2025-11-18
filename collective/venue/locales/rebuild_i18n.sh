bc89a997304d46f8a153cfd9494cc3220775c689
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-11-19 00:35:33 +0100

    Added more improvements about the i18n support.

--- collective/venue/locales/rebuild_i18n.sh
+++ collective/venue/locales/rebuild_i18n.sh
@@ -1,5 +1,6 @@
 i18ndude rebuild-pot --pot ./collective.venue.pot --create collective.venue ../ || exit 1
-i18ndude rebuild-pot --pot ./plone.pot --create plone ../ || exit 1
+i18ndude rebuild-pot --pot ./plone.pot --create plone ../profiles/ || exit 1
+i18ndude merge --pot ./plone.pot --merge ./merge-plone.pot || exit 1
 i18ndude sync --pot ./collective.venue.pot ./*/LC_MESSAGES/collective.venue.po
 i18ndude sync --pot ./plone.pot ./*/LC_MESSAGES/plone.po
 
